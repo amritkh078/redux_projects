4cc536f4fc0ccdef1e78e07774774d2439ac2ebe
amritkh078 <amritkhatiwada078@gmail.com>
2023-01-24 21:59:07 +0545

    toolkit

--- Redux/src/main.jsx
+++ Redux/src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { configureStore } from '@reduxjs/toolkit'
 
+const store = configureStore({
+  
+}) 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
