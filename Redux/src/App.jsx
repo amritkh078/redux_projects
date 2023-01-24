b7b16093330e214ceef252134f73083991db22b9
amritkh078 <amritkhatiwada078@gmail.com>
2023-01-24 15:49:22 +0545

    redux

---
+++ Redux/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Profile from './components/Profile'
+import Login from './components/Login'
+
+const App = () => {
+  return (
+    <>
+    <Login/>
+    <Profile/>
+    </>
+  )
+}
+
+export default App
