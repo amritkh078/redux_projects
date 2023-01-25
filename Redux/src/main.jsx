585d8b7ee99601e4820cc2a8005e432b0b1a1845
amritkh078 <amritkhatiwada078@gmail.com>
2023-01-25 18:02:13 +0545

    reducer

--- Redux/src/main.jsx
+++ Redux/src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux' // provider is a component that wraps the app and gives it access to the store
 
 const store = configureStore({
-  
+  reducer:{}  // reducer is a function that takes the current state and an action and returns the next state
 }) 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
