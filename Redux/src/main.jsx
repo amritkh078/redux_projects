149271d9f8a9c52f4a9fbb91ca0565e3808b20f2
amritkh078 <amritkhatiwada078@gmail.com>
2023-01-25 18:15:46 +0545

    user reducer

--- Redux/src/main.jsx
+++ Redux/src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import userReducer from './features/userSlice'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux' // provider is a component that wraps the app and gives it access to the store
 
 const store = configureStore({
-  reducer:{}  // reducer is a function that takes the current state and an action and returns the next state
+  reducer:{
+    user: userReducer
+  } 
 }) 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
